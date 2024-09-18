aa62985161483a5631583fc0ab57b810876c66cb
GUERRIC KERNYUY NSOSAH <guerrickernyuy@gmail.com>
2024-09-18 21:03:13 +0000

    add function structure to js file

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+function startGame() {
+
+}
+
+function loadGrid() {
+
+}
+
+function flipImage() {
+
+}
+
+function checkForMatch() {
+
+}
+
+
+function startTimer() {
+
+}
+
+function nextLevel() {
+
+}
+
+function reloadLevel() {
+
+}
+
+function resetGame() {
+
+}
+
+function updateFooter() {
+
+}
+
